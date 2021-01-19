583b94ade86e66a26995f328957a0687f63a4f03
murali_m_v <sheshasai@gmail.com>
2021-01-19 15:25:36 +0530

    Commit till Section 9

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using System.Collections.Generic;
+using API.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class UsersController : BaseApiController
+    {
+        //private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users = await _userRepository.GetUsersAsync();
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+        
+            //var usersToReturn = _mapper.Map<MemberDto>(users);
+            return Ok(usersToReturn);
+        }
+
+
+        // api/users/3 
+  
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            return _mapper.Map<MemberDto>(user);
+        }
+
+    }
+}
